70a5204e7f205cebd2dfc299caaa048f8f60fcac
Colby Butler <ColbyWanShinobi@users.noreply.github.com>
2025-02-05 20:47:52 -0500

    Add files via upload

---
+++ files/system/usr/sbin/blueox-os-unverified-update.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+rpm-ostree rebase ostree-unverified-registry:ghcr.io/colbywanshinobi/blueox-os:gnome
---
+++ files/system/usr/sbin/blueox-os-verified-update.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+rpm-ostree rebase ostree-image-signed:docker://ghcr.io/colbywanshinobi/blueox-os:gnome
