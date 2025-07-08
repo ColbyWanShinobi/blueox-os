7bf5ad200320e0931c09fc5d63ab14ea8bc40f62
Colby Butler <colby@owljet.com>
2025-07-08 12:29:02 -0400

    Updated script for future research

--- files/scripts/remove-select-gnome-shell-system-extensions.sh
+++ files/scripts/remove-select-gnome-shell-system-extensions.sh
@@ -7,9 +7,16 @@ set -oue pipefail
 
 # Create an array of shell extension names to be removed
 declare -a extensions=(
-  'gnome-shell-extension-compiz*'
-  'gnome-shell-extension-gamerzilla*'
-  'gnome-shell-extension-hotedge*'
+  'gnome-shell-extension-burn-my-windows-46-1.fc42.noarch'
+  'gnome-shell-extension-compiz-alike-magic-lamp-effect-0.0.git.5689.e65cf76b-1.fc42.noarch'
+  'gnome-shell-extension-compiz-windows-effect-0.0.git.5689.e65cf76b-1.fc42.noarch'
+  'gnome-shell-extension-desktop-cube-28-1.fc42.noarch'
+  'gnome-shell-extension-hanabi-0.0.git.403.ef70e818-1.fc42.noarch'
+  'gnome-shell-extension-hotedge-0.0.git.67.2fb8340a-1.fc42.noarch'
+  'gnome-shell-extension-launch-new-instance-48.2-1.fc42.noarch'
+  'gnome-shell-extension-places-menu-48.2-1.fc42.noarch'
+  'gnome-shell-extension-bazzite-menu-0.0.git.350.a5ef7dc6-1.fc42.noarch'
+  'gnome-shell-extension-window-list-48.2-1.fc42.noarch'
 )
 
 # Loop through each extension and remove it
