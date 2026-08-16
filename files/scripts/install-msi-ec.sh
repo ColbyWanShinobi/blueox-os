0473749f74a1fe1a178244bbc3183322791ef7db
Colby Butler <colby@owljet.com>
2026-08-15 23:29:21 -0400

    fixed msi-ec

--- files/scripts/install-msi-ec.sh
+++ files/scripts/install-msi-ec.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-MSI_EC_COMMON_RPM='https://github.com/ColbyWanShinobi/msi-ec/releases/download/v0.13/msi-ec-kmod-common-0.13-1.fc44.noarch.rpm'
-AKMOD_MSI_EC_RPM='https://github.com/ColbyWanShinobi/msi-ec/releases/download/v0.13/akmod-msi-ec-0.13-1.fc44.x86_64.rpm'
+MSI_EC_COMMON_RPM='https://github.com/ColbyWanShinobi/msi-ec/releases/download/v0.13/msi-ec-kmod-common-0.13-2.fc44.noarch.rpm'
+AKMOD_MSI_EC_RPM='https://github.com/ColbyWanShinobi/msi-ec/releases/download/v0.13/akmod-msi-ec-0.13-2.fc44.x86_64.rpm'
 
 if ! command -v dnf >/dev/null 2>&1; then
   echo 'This installer requires dnf.' >&2
